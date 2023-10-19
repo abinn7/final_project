6623cece2ba3b0c37ddcea26a5501dee62ab13b4
abinn7 <129936946+abinn7@users.noreply.github.com>
2023-10-19 12:35:48 +0530

    Update Emotion_dectection.py

--- Emotion_dectection.py
+++ Emotion_dectection.py
@@ -13,12 +13,21 @@ def emotion_detector(text_to_analyze):
     }
 
     # Send a POST request to the Watson NLP Library
-    response = requests.post(url, headers=headers, json=input_json)
+    response = requests.post(URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+Input json: { "raw_document": { "text": text_to_analyse } })
 
     # Check for a successful response
     if response.status_code == 200:
         # Parse and return the result (you may need to adapt this based on the actual response format)
-        result = response.json()
+        result = response.json({
+'anger': anger_score,
+'disgust': disgust_score,
+'fear': fear_score,
+'joy': joy_score,
+'sadness': sadness_score,
+'dominant_emotion': '<name of the dominant emotion>'
+})
         return result
     else:
         # Handle errors if needed
